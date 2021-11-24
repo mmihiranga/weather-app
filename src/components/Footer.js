69b5ef204c6580050da37f67277471f16bd5c0b6
mmihiranga <madhuramihiranga6@gmail.com>
2021-11-24 19:28:09 +0530

    code refactor

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
-import React ,{useEffect,useState}from 'react'
+import React, { useEffect, useState } from "react";
 
-const Footer=()=>{
-return(
+const Footer = () => {
+  return (
     <div>
-     <div className="footer">2021 Fidenz Technologies</div>
+      <div className="footer">2021 Fidenz Technologies</div>
     </div>
-)
-}
+  );
+};
 export default Footer;
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { createBrowserHistory } from "history";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Home";
 import OverView from "./OverView";
 
 const hist = createBrowserHistory();
 
 const routes = () => (
-<Router history={hist}>
-<Route  exact path="/" component={Home}/>
-<Route  exact path="/overview" component={OverView}/>
-</Router>
+  <Router history={hist}>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/overview" component={OverView} />
+  </Router>
 );
 export default routes;
